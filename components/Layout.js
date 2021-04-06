214eed3340bc54dbc48265dccf5d6422eca56eb5
Svitlana Kryzhanivska <lana19333@gmail.com>
2021-04-06 11:26:20 +0300

    Add Google analytics

--- components/Layout.js
+++ components/Layout.js
@@ -14,6 +14,20 @@ const Layout = ({ children }) => {
                 <meta name="theme-color" content="#000000" />
                 <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo.png" />
                 <link rel="icon" href="/favicon.ico" />
+                {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+                <script async src="https://www.googletagmanager.com/gtag/js?id=UA-193690651-1">
+                </script>
+                <script dangerouslySetInnerHTML={
+                    {
+                        __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'UA-193690651-1');
+                    `}
+                }>
+                </script>
             </Head>
             <NavBar />
             {children}
