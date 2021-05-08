9f843cc09130d0ed311350913770c6f848b158cf
Svitlana Kryzhanivska <lana19333@gmail.com>
2021-05-08 12:56:15 +0300

    Add Google Tag Manager

--- components/Layout.js
+++ components/Layout.js
@@ -7,6 +7,7 @@ const Layout = ({ children }) => {
     return (
         <>
             <Head>
+
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link href="https://fonts.googleapis.com/css?family=Montserrat|Didact+Gothic|Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800|Nunito+Sans|Old+Standard+TT:700|Raleway|Source+Sans+Pro:600&display=swap"
                     rel="stylesheet" />
@@ -28,10 +29,27 @@ const Layout = ({ children }) => {
                     `}
                 }>
                 </script>
+                {/* <!-- Google Tag Manager --> */}
+                <script dangerouslySetInnerHTML={{
+                        __html: `function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                        })(window,document,'script','dataLayer','GTM-NTW5S2R');`
+                    }}>   
+                </script>
             </Head>
-            <NavBar />
-            {children}
-            <Footer />
+            <body>
+            {/* <!-- Google Tag Manager (noscript) --> */}
+            <noscript dangerouslySetInnerHTML={{
+                    __html:`<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NTW5S2R"
+                    height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+                }}>
+            </noscript>
+                <NavBar />
+                {children}
+                <Footer />
+            </body>
         </>
     )
 }
