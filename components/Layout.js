c98c6624325dc3a0b6bd914f977b4bc809441071
Svitlana Kryzhanivska <lana19333@gmail.com>
2021-05-09 22:23:25 +0300

    Fix Google Tag Manager

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-
 import NavBar from './NavBar';
 import Footer from './Footer';
 
@@ -28,27 +27,10 @@ const Layout = ({ children }) => {
                     `}
                 }>
                 </script>
-                {/* <!-- Google Tag Manager --> */}
-                <script dangerouslySetInnerHTML={{
-                        __html: `function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                        })(window,document,'script','dataLayer','GTM-NTW5S2R');`
-                    }}>   
-                </script>
             </Head>
-            <body>
-            {/* <!-- Google Tag Manager (noscript) --> */}
-            <noscript dangerouslySetInnerHTML={{
-                    __html:`<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NTW5S2R"
-                    height="0" width="0" style="display:none;visibility:hidden"></iframe>`
-                }}>
-            </noscript>
-                <NavBar />
-                {children}
-                <Footer />
-            </body>
+            <NavBar />
+            {children}
+            <Footer />
         </>
     )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import { ThemeProvider } from 'styled-components';
 import { Provider } from 'react-redux';
+import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
 
 import { GlobalStyle } from '../src/theme/GlobalStyle';
 import { theme } from '../src/theme/Theme';
@@ -7,6 +9,9 @@ import store from '../store';
 import Layout from '../components/Layout';
 
 function LizenaApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-NTW5S2R' });
+}, []);
   return <>
   <Provider store={store}>
     <GlobalStyle />
